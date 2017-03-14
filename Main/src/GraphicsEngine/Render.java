c9b6cadf305f8ca3a2733b031dedc9992403f830
Tristan Charpentier <addict1@outlook.com>
2017-03-14 14:37:05 -0400

    Added folders

---
+++ Main/src/GraphicsEngine/Render.java
@@ -0,0 +1,12 @@
+package GraphicsEngine;
+
+/**
+ * Created by tristan on 3/14/2017.
+ */
+public class Render
+{
+    public Render()
+    {
+
+    }
+}
---
+++ Main/src/Main/Engine.java
@@ -0,0 +1,14 @@
+package Main;
+
+import GraphicsEngine.Render;
+
+/**
+ * Created by tristan on 3/14/2017.
+ */
+public class Engine
+{
+    public Engine()
+    {
+        Render render = new Render();
+    }
+}
